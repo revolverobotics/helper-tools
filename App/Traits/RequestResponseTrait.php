8a314fdaed547253d157ac9b4e098868bbe4a172
Timothy Huang <heisian@gmail.com>
2016-04-13 22:48:29 -0700

    allow null to be passed to makeResponse (arg1), syntax fixing for phpcs

--- App/Traits/RequestResponseTrait.php
+++ App/Traits/RequestResponseTrait.php
@@ -1,10 +1,8 @@
 <?php
-
 /*
     For providing uniform request/response handling across our different
     controllers
 */
-
 namespace App\Submodules\ToolsLaravelMicroservice\App\Traits;
 
 use Helper;
@@ -35,8 +33,10 @@ trait RequestResponseTrait
 
     public function makeResponse($data, $message="")
     {
-        if (!is_array($data))
-            throw new \FatalErrorException('Argument 1 (data) of makeResponse must be passed as an array.');
+        if (!is_array($data) && !is_null($data))
+            throw new \FatalErrorException(
+                'Argument 1 (data) of makeResponse must be passed as an array.'
+            );
 
         $this->response = $data;
 
@@ -44,18 +44,23 @@ trait RequestResponseTrait
             $message = implode(" ", $message);
 
         if (!is_string($message))
-            throw new \FatalErrorException('Argument 2 (message) must be a string or an array.');
+            throw new \FatalErrorException(
+                'Argument 2 (message) must be a string or an array.'
+            );
 
         $this->response['message'] = $message;
     }
 
-    public function backendRequest($service, $endpoint, $data=null, $method=null, $property=true)
-    {
-        if (is_null($data))
+    public function backendRequest($service, $endpoint, $data=null,
+        $method=null, $property=true
+    ) {
+        if (is_null($data)) {
             $data = $this->request->all();
+        }
 
-        if (is_null($method))
+        if (is_null($method)) {
             $method = $this->request->method();
+        }
 
         $backendResponse = Helper::sendRequest(
             $method,
@@ -68,16 +73,18 @@ trait RequestResponseTrait
             If true, store the response in our $this->response property to be
             accessible by other traits and the host controller:
         */
-        if ($property === true)
-            $this->response = $backendResponse;
-
-        // Otherwise, return the response so we can use it in a more local scope
-        else
+        if (!$property) {
             return $backendResponse;
+        }
+
+        $this->response = $backendResponse;
     }
 
     public function backendResponse()
     {
-        return Helper::prettyJson($this->response['code'], $this->response['json']);
+        return Helper::prettyJson(
+            $this->response['code'],
+            $this->response['json']
+        );
     }
 }
