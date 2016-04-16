acc52296fcd6482bcb16456fa0e0095e05b5cc20
heisian <tim@revolverobotics.com>
2016-04-16 16:30:51 -0700

    arrayaccess implements

--- App/Classes/BackendModel.php
+++ App/Classes/BackendModel.php
@@ -45,14 +45,9 @@ abstract class BackendModel implements ArrayAccess
      *
      * @var string
      */
-    public function __construct(
-        string $modelName,
-        string $service,
-        BackendRequest $connection
-    ) {
-        $this->modelName = $modelName;
-
-        $this->connection = $connection($service);
+    public function __construct(BackendRequest $connection)
+    {
+        $this->connection = $connection($this->service);
     }
 
     public function get($data)
@@ -98,5 +93,50 @@ abstract class BackendModel implements ArrayAccess
         if ($response->code() == 200) {
             // $this->modelData = null
         }
+    }
+
+    /**
+     * Determine if the given attribute exists.
+     *
+     * @param  mixed  $offset
+     * @return bool
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->$offset);
+    }
+
+    /**
+     * Get the value for a given offset.
+     *
+     * @param  mixed  $offset
+     * @return mixed
+     */
+    public function offsetGet($offset)
+    {
+        return $this->$offset;
+    }
+
+    /**
+     * Set the value for a given offset.
+     *
+     * @param  mixed  $offset
+     * @param  mixed  $value
+     * @return void
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->$offset = $value;
+    }
+
+    /**
+     * Unset the value for a given offset.
+     *
+     * @param  mixed  $offset
+     * @return void
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->$offset);
     }
 }
