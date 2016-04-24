923354d27098d3259a382bc8a4458be02c82ec31
heisian <tim@revolverobotics.com>
2016-04-23 20:30:23 -0700

    improve CustomController, enable ANSI color output for Log

--- App/Traits/ModelHelperTrait.php
+++ App/Traits/ModelHelperTrait.php
@@ -4,8 +4,25 @@ namespace App\Submodules\ToolsLaravelMicroservice\App\Traits;
 
 trait ModelHelperTrait
 {
+    /**
+     * Results from the magicSearch method.
+     *
+     * @var Illuminate\Pagination\LengthAwarePaginator
+     */
     protected $magicResult;
 
+    /**
+     * Searches for a model based on any number of input received.
+     * Searchable columns/attributes are specified in the model's
+     * $queryable property.
+     *
+     * @param  array $input
+     * @param  int $pagination
+     * @return Illuminate\Pagination\LengthAwarePaginator
+     *
+     * @throws Psy\Exception\FatalErrorException;
+     * @throws Symfony\Component\HttpKernel\Exception\BadRequestHttpException
+     */
     public function magicSearch(array $input, $pagination = 5)
     {
         if (is_null($this->queryable)) {
@@ -44,6 +61,26 @@ trait ModelHelperTrait
         return $result;
     }
 
+    /**
+     * Returns any models found with the magicSearch method as an array.
+     *
+     * @return array
+     */
+    public function extract()
+    {
+        if (!is_null($this->magicResult)) {
+            return $this->magicResult->toArray()['data'];
+        }
+        return collect([$this->toArray()]);
+    }
+
+    /**
+     * Replaces the 'data' key name returned by LengthAwarePaginator
+     * with a model-centric term, e.g., 'users'
+     *
+     * @param  string $override
+     * @return array
+     */
     public function dataKeyToModelName($override = null)
     {
         if (!isset($this->magicResult)) {
@@ -51,6 +88,8 @@ trait ModelHelperTrait
         }
 
         $result = $this->magicResult->toArray();
+
+        $this->trimPagination($result);
 
         if (!isset($result['data'])) {
             return false;
@@ -65,24 +104,25 @@ trait ModelHelperTrait
         $result[$name] = $result['data'];
         unset($result['data']);
 
-        return $this->trimPagination($result);
-    }
-
-    protected function trimPagination($result)
-    {
-        if ($result['last_page'] == 1) {
-            unset($result['per_page']);
-            unset($result['current_page']);
-            unset($result['last_page']);
-            unset($result['next_page_url']);
-            unset($result['prev_page_url']);
-            unset($result['from']);
-            unset($result['to']);
-        }
-
         return $result;
     }
 
+    /**
+     * Trims excess pagination data if there is only one page.
+     *
+     * @param  $result array
+     * @return void
+     */
+    protected function trimPagination(&$result)
+    {
+        $result = array_only($result, ['total', 'data']);
+    }
+
+    /**
+     * Returns any models found from the magicSearch method
+     *
+     * @return Illuminate\Pagination\LengthAwarePaginator
+     */
     public function getResult()
     {
         if (!isset($this->magicResult)) {
@@ -90,5 +130,27 @@ trait ModelHelperTrait
         }
 
         return $this->magicResult;
+    }
+
+    /**
+     * Counts the number of models we expected to find and
+     * throws an exception if it is not correct.
+     *
+     * @param  int $n
+     * @return void
+     *
+     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+     */
+    public function expected(int $n)
+    {
+        // Number of models expected to be found.
+        // Throw an exception otherwise
+        $count = count($this->extract());
+        if ($count != $n) {
+            throw new \ModelNotFoundException(
+                'Incorrect number of models found in collection. '.
+                "Expected: $n | Found: $count"
+            );
+        }
     }
 }
