f965668386706d60c4d6a8117ecd2a7c23e4f9a7
Timothy Huang <tim@revolverobotics.com>
2016-12-01 13:47:36 -0800

    allow extension of an auth() method that checks any auth requirements before allowing access to basic unit test routes

--- src/Http/Controllers/BaseUnitTestController.php
+++ src/Http/Controllers/BaseUnitTestController.php
@@ -11,8 +11,10 @@ class BaseUnitTestController extends CustomController
 {
     use BaseUnitTestTrait;
 
-    public function postPreTest()
+    public function preTest()
     {
+        $this->auth();
+
         $this->runCustomChecks();
 
         $this->makeResponse([], 'Pre-test finished.');
@@ -20,26 +22,37 @@ class BaseUnitTestController extends CustomController
         return $this->success();
     }
 
-    public function getCache()
+    public function testGetCache()
     {
+        $this->auth();
+
         $this->makeResponse(['data' => Cache::get('PHPUnitTest')]);
 
         return $this->success();
     }
 
-    public function putCache()
+    public function testPutCache()
     {
+        $this->auth();
+
         Cache::put('PHPUnitTest', 'testData', 60);
 
         return $this->success();
     }
 
-    public function postCleanup()
+    public function cleanup()
     {
+        $this->auth();
+
         $this->performCleanupOperations();
 
         $this->makeResponse([], 'Cleanup finished.');
 
         return $this->success();
+    }
+
+    protected function auth()
+    {
+        // Extend with any auth logic needed to access these routes
     }
 }
