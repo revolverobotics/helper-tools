78048318ca2a9f7b401dc31f656651be37ca40c8
Timothy Huang <tim@revolverobotics.com>
2017-02-17 19:17:01 -0800

    update request validator to latest spec of laravel 5.4

--- src/Traits/RequestValidatorTrait.php
+++ src/Traits/RequestValidatorTrait.php
@@ -7,7 +7,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\UrlGenerator;
 use Illuminate\Contracts\Validation\Factory;
 use Illuminate\Contracts\Validation\Validator;
-use Illuminate\Http\Exception\HttpResponseException;
+use Illuminate\Validation\ValidationException;
 
 trait RequestValidatorTrait
 {
@@ -17,6 +17,24 @@ trait RequestValidatorTrait
      * @var string
      */
     protected $validatesRequestErrorBag;
+
+    /**
+     * Run the validation routine against the given validator.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator|array  $validator
+     * @param  \Illuminate\Http\Request|null  $request
+     * @return void
+     */
+    public function validateWith($validator, Request $request = null)
+    {
+        $request = $request ?: app('request');
+        if (is_array($validator)) {
+            $validator = $this->getValidationFactory()->make($request->all(), $validator);
+        }
+        if ($validator->fails()) {
+            $this->throwValidationException($request, $validator);
+        }
+    }
 
     /**
      * Validate the given request with the given rules.
@@ -32,7 +50,6 @@ trait RequestValidatorTrait
     public function validate(array $rules, array $messages = [], array $customAttributes = [])
     {
         $validator = $this->getValidationFactory()->make(request()->all(), $rules, $messages, $customAttributes);
-
         if ($validator->fails()) {
             throw new \BadRequestHttpException(implode(" ", $validator->errors()->all()));
         }
@@ -41,7 +58,6 @@ trait RequestValidatorTrait
     public function validateStock(Request $request, array $rules, array $messages = [], array $customAttributes = [])
     {
         $validator = $this->getValidationFactory()->make($request->all(), $rules, $messages, $customAttributes);
-
         if ($validator->fails()) {
             $this->throwValidationException($request, $validator);
         }
@@ -57,86 +73,14 @@ trait RequestValidatorTrait
      * @param  array  $customAttributes
      * @return void
      *
-     * @throws \Illuminate\Http\Exception\HttpResponseException
+     * @throws \Illuminate\Validation\ValidationException
      */
-    public function validateWithBag(
-        $errorBag,
-        Request $request,
-        array $rules,
-        array $messages = [],
-        array $customAttributes = []
-    ) {
+    public function validateWithBag($errorBag, Request $request, array $rules, array $messages = [], array $customAttributes = [])
+    {
         $this->withErrorBag($errorBag, function () use ($request, $rules, $messages, $customAttributes) {
-            $this->validateStock($request, $rules, $messages, $customAttributes);
+            $this->validate($request, $rules, $messages, $customAttributes);
         });
     }
-
-    /**
-     * Throw the failed validation exception.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Illuminate\Contracts\Validation\Validator  $validator
-     * @return void
-     *
-     * @throws \Illuminate\Http\Exception\HttpResponseException
-     */
-    protected function throwValidationException(Request $request, $validator)
-    {
-        throw new HttpResponseException($this->buildFailedValidationResponse(
-            $request,
-            $this->formatValidationErrors($validator)
-        ));
-    }
-
-    /**
-     * Create the response for when a request fails validation.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  array  $errors
-     * @return \Illuminate\Http\Response
-     */
-    protected function buildFailedValidationResponse(Request $request, array $errors)
-    {
-        if ($request->ajax() || $request->wantsJson()) {
-            return new JsonResponse($errors, 422);
-        }
-
-        return redirect()->to($this->getRedirectUrl())
-                        ->withInput($request->input())
-                        ->withErrors($errors, $this->errorBag());
-    }
-
-    /**
-     * Format the validation errors to be returned.
-     *
-     * @param  \Illuminate\Contracts\Validation\Validator  $validator
-     * @return array
-     */
-    protected function formatValidationErrors(Validator $validator)
-    {
-        return $validator->errors()->getMessages();
-    }
-
-    /**
-     * Get the URL we should redirect to.
-     *
-     * @return string
-     */
-    protected function getRedirectUrl()
-    {
-        return app(UrlGenerator::class)->previous();
-    }
-
-    /**
-     * Get a validation factory instance.
-     *
-     * @return \Illuminate\Contracts\Validation\Factory
-     */
-    protected function getValidationFactory()
-    {
-        return app(Factory::class);
-    }
-
     /**
      * Execute a Closure within with a given error bag set as the default bag.
      *
@@ -147,12 +91,50 @@ trait RequestValidatorTrait
     protected function withErrorBag($errorBag, callable $callback)
     {
         $this->validatesRequestErrorBag = $errorBag;
-
         call_user_func($callback);
-
         $this->validatesRequestErrorBag = null;
     }
-
+    /**
+     * Throw the failed validation exception.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function throwValidationException(Request $request, $validator)
+    {
+        throw new ValidationException($validator, $this->buildFailedValidationResponse(
+            $request, $this->formatValidationErrors($validator)
+        ));
+    }
+    /**
+     * Create the response for when a request fails validation.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  array  $errors
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function buildFailedValidationResponse(Request $request, array $errors)
+    {
+        if ($request->expectsJson()) {
+            return new JsonResponse($errors, 422);
+        }
+        return redirect()->to($this->getRedirectUrl())
+                        ->withInput($request->input())
+                        ->withErrors($errors, $this->errorBag());
+    }
+    /**
+     * Format the validation errors to be returned.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     * @return array
+     */
+    protected function formatValidationErrors(Validator $validator)
+    {
+        return $validator->errors()->getMessages();
+    }
     /**
      * Get the key to be used for the view error bag.
      *
@@ -161,5 +143,23 @@ trait RequestValidatorTrait
     protected function errorBag()
     {
         return $this->validatesRequestErrorBag ?: 'default';
+    }
+    /**
+     * Get the URL we should redirect to.
+     *
+     * @return string
+     */
+    protected function getRedirectUrl()
+    {
+        return app(UrlGenerator::class)->previous();
+    }
+    /**
+     * Get a validation factory instance.
+     *
+     * @return \Illuminate\Contracts\Validation\Factory
+     */
+    protected function getValidationFactory()
+    {
+        return app(Factory::class);
     }
 }
